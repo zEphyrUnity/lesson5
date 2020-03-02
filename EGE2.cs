aed50e4219ec5c6235de17efa3763f134fbb8b9c
Igor Papkin <stelzz@yandex.ru>
2020-03-02 04:32:12 +0300

    third commit

--- EGE2.cs
+++ EGE2.cs
@@ -1,6 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Text;
+
+/* Папкин Игорь
+ * На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов некоторой средней школы. 
+ * В первой строке сообщается количество учеников N, которое не меньше 10, но не превосходит 100, каждая из следующих N строк имеет следующий формат:
+ * <Фамилия> <Имя> <оценки>,
+ * где <Фамилия> — строка, состоящая не более чем из 20 символов, <Имя> — строка, состоящая не более чем из 15 символов, 
+ * <оценки> — через пробел три целых числа, соответствующие оценкам по пятибалльной системе. <Фамилия> и <Имя>, а также <Имя> и <оценки> разделены одним пробелом. Пример входной строки:
+ * Иванов Петр 4 5 3 */
 
 namespace EGE2
 {
@@ -21,10 +30,17 @@ namespace EGE2
             }
 
             StreamReader sr = new StreamReader(path);
-
+            //Проверка первой строки на соответствие формата данных
             if (!Int32.TryParse(sr.ReadLine(), out studentsNum))
             {
                 Console.WriteLine("Неправильно сформирован входной файл");
+            }
+
+            //Проверка количества учеников по первой строке
+            if (studentsNum < 10 || studentsNum > 100)
+            {
+                Console.WriteLine("Количество учеников должно быть не меньше 10 и не больше 100");
+                return;
             }
 
             string[,] students = new string[studentsNum, 3];
@@ -32,6 +48,7 @@ namespace EGE2
             int k = 0;
             double average;
 
+            //Чтение данных учеников из файла 
             while (!sr.EndOfStream)
             {
                 text = sr.ReadLine();
@@ -41,11 +58,42 @@ namespace EGE2
                 for (int i = 0; i < nextString.Length; i++)
                     nextString[i] = nextString[i].Trim('\r');
 
+                //Проверка строки на соответствие данных формату "Vasya Petrov 5 5 5"
+                for(int i = 0; i < nextString.Length; i ++)
+                {
+                    if(i == 0)
+                    {
+                        if (!Regex.IsMatch(nextString[i], @"\b[A-Za-z]{2,20}\b"))
+                        {
+                            Console.WriteLine("Данные введены не верно, фамилия должна быть не менее двух символов и не более 20, и не содержать цифры");
+                            return;
+                        }
+                    }
+                    else if (i == 1)
+                    {
+                        if (!Regex.IsMatch(nextString[i], @"\b[A-Za-z]{2,15}\b"))
+                        {
+                            Console.WriteLine("Данные введены не верно, имя должно быть не менее двух символов и не более 15, и не содержать цифры");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(nextString[i], @"\D"))
+                        {
+                            Console.WriteLine("Данные введены не верно, введите число");
+                            return;
+                        }
+                    }
+                }
+
+                //Ввод имени и фамилии в из массива строки в двумерный массив
                 for(int i = 0; i < students.GetLength(1) - 1; i++)
                 {
                     students[k, i] = nextString[i];
                 }
 
+                //Расчет среднего бала и ввод среднего балла в двумерный массив
                 average = 0;
                 for (int i = nextString.Length - 3; i < nextString.Length; i++)
                 {
@@ -57,6 +105,7 @@ namespace EGE2
                 k++;
             }
 
+            //Сортировка по среднему балу
             for (int i = 0; i < students.GetLength(0) - 1; i++)
             {
                 for (int j = i + 1; j < students.GetLength(0); j++)
@@ -78,6 +127,7 @@ namespace EGE2
                 }
             }
 
+            //Вывод результатов
             for (int i = 0; i < students.GetLength(0); i++)
             {
                 for(int j = 0; j < students.GetLength(1); j++)
@@ -92,15 +142,6 @@ namespace EGE2
                     {
                         Console.Write($"{ students[i, j]} ");
                     }
-                }
-                Console.WriteLine();
-            }
-
-            for (int i = 0; i < students.GetLength(0); i++)
-            {
-                for (int j = 0; j < students.GetLength(1); j++)
-                {
-                    Console.Write($"{ students[i, j]} ");
                 }
                 Console.WriteLine();
             }
